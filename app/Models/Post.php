347e39184cded3989348bffa8f1ee67acd7b8b79
Daniel Languiller <dlanguiller@opticomm.net.au>
2021-11-03 20:45:27 +1100

    first and last

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title', 'slug', 'likes', 'content',
+    ];
+
+    protected $casts = [
+        'likes' => 'integer',
+    ];
+}
